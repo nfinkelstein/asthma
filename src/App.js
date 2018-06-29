0524396b22c639483a7fe934d7e67e4b1e71d40a
Nicolas Finkelstein <nico@Nicolass-MacBook-Pro.local>
2018-06-28 20:35:03 -0500

    Added tabs

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import NavItem from '../node_modules/react-bootstrap/lib/NavItem'
 import MenuItem from '../node_modules/react-bootstrap/lib/MenuItem'
 import NavDropdown from '../node_modules/react-bootstrap/lib/NavDropdown'
 
+import Tabs from '../node_modules/react-bootstrap/lib/Tabs'
+import Tab from '../node_modules/react-bootstrap/lib/Tab'
 
 
 
@@ -23,41 +25,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hackathon Project</h1>
         </header>
-
-        <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#brand">React-Bootstrap</a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} href="#">
-        Link
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Link
-      </NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
-    </Nav>
-    <Nav pullRight>
-      <NavItem eventKey={1} href="#">
-        Link Right
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Link Right
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>;
-        <FormParent />
+        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+          <Tab eventKey={1} title={<span><i className="material-icons">home</i></span>}>
+            Tab 1 content
+          </Tab>
+          <Tab eventKey={2} title={<span><i className="material-icons">insert_chart</i></span>}>
+          </Tab>
+          <Tab eventKey={3} title={<span><i className="material-icons">storage</i></span>}>
+            <FormParent/>
+          </Tab>
+            <Tab eventKey={4} title={<span><i className="material-icons">settings</i></span>}>
+            Tab 3 content
+          </Tab>
+        </Tabs>
       </div>
     );
   }
